15e4715e798d5fbcde8c5e91b846fa5c18bf50c5
akshata <akshata2018p@gamil.com>
2023-03-05 20:24:48 +0530

    [akshata]Add:First Name

---
+++ RegexProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*-----REGEX-----*");
+            User user = new User();
+            user.ValidateFirstName("Rohan");
+
+            Console.ReadKey();
+        }
+    }
+}

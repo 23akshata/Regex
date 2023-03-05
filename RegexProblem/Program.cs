0b152eab2b631e0fbc72cc5bf4cbb84a0888d97d
akshata <akshata2018p@gamil.com>
2023-03-05 21:46:46 +0530

    [akshata]Add:Passward Rule 4 All Cases

--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -19,6 +19,8 @@ namespace RegexProblem
             user.ValidatePasswordRule1("123455611");
             user.ValidatePasswordRule2("12345678A");
             user.ValidatePasswordRule3("abcdefg4C");
+            user.ValidatePasswordRule4("avcd&sfd4C");
+
             Console.ReadKey();
         }
     }
